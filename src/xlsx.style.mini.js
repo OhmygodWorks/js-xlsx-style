7e3f69500f43432dc5de57061bd3b4a591fdb4a4
叶坷 <ohmygodworks@outlook.com>
2019-08-29 18:28:16 +0800

    添加了一个方便处理复数cell的utils方法

--- src/xlsx.style.mini.js
+++ src/xlsx.style.mini.js
@@ -8405,6 +8405,20 @@ utils.sheet_set_array_formula = function(ws, range, formula) {
 	return ws;
 };
 
+utils.sheet_set_ranged_cells = function(ws, range, operations) {
+	let rng = (typeof range != "string" ? range : safe_decode_range(range)).freeze();
+	let rngstr = typeof range == "string" ? range : encode_range(range);
+	for(let R = rng.s.r; R <= rng.e.r; ++R) for(let C = rng.s.c; C <= rng.e.c; ++C) {
+		let cell = ws_get_cell_stub(ws, R, C);
+		try{
+			operations(R, C, cell, rng, rngstr);
+		}catch(e){
+			console.error("error when operating cell: " + (e.message || e));
+		}
+	}
+	return ws;
+};
+
 return utils;
 })(utils);
 
